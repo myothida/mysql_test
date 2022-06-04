5ef088b518aab72b985dfdd286adebda0f6ac36a
Dr Myo Thida <88795729+myothida@users.noreply.github.com>
2022-06-04 19:07:50 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -22,36 +22,52 @@ from dash import Input, Output, State, html
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
-from sqlalchemy import create_engine
-
 
 # In[2]:
 
 
-db_str = "mysql+mysqldb://dspuser:dsp@2021!@202.144.157.56/dsp_db" #$@L21myo
-db_connection = create_engine(db_str)
+#from sqlalchemy import create_engine
+#db_str = "mysql+mysqldb://dspuser:dsp@2021!@202.144.157.56/dsp_db" 
+#db_connection = create_engine(db_str)
 
 
 # In[3]:
 
 
-# using psycopg2
-
-#pg_engine = "user='{}' password='{}' host='{}' dbname='{}'".format(user, password, host, db)
-#conn = psycopg2.connect(pg_engine)
+import pymysql
+pg_engine = "user='{}' password='{}' host='{}' dbname='{}'".format('dspuser','dsp@2021!', '202.144.157.56', 'dsp_db')
+db_connection = pymysql.connect(host = '202.144.157.56', user = 'dspuser', 
+                                password = 'dsp@2021!', db = 'dsp_db')
+cur = db_connection.cursor()
 
 
 # In[4]:
 
 
-sql_query = "SELECT * FROM student_dtls JOIN students_courses ON students_courses.student_id=student_dtls.student_id JOIN course_master ON course_master.id=students_courses.course_id JOIN dessung_profile ON dessung_profile.cid=student_dtls.cid JOIN dzongkhag_master ON dzongkhag_master.id = dessung_profile.dzongkhag_id JOIN qualifications_master ON qualifications_master.id=dessung_profile.qualification_id;"
-result = db_connection.execute(sql_query)
-result =pd.DataFrame(result)
-result.rename(columns={8:'CID #',10:'DOB',11:'Email',14:'Name',27:'Programme',35:'DID',38:'Sex',47:'Dzongkhag',49:'Qualification'},inplace=True)
-df=result[['CID #','Name','Programme','DID','Sex','Dzongkhag','Qualification']]
+# using psycopg2
+#import psycopg2
+#pg_engine = "user='{}' password='{}' host='{}' dbname='{}'".format('dspuser','dsp@2021!', '202.144.157.56', 'dsp_db')
+#conn = psycopg2.connect(pg_engine)
 
 
 # In[5]:
+
+
+sql_query = "SELECT * FROM student_dtls JOIN students_courses ON students_courses.student_id=student_dtls.student_id JOIN course_master ON course_master.id=students_courses.course_id JOIN dessung_profile ON dessung_profile.cid=student_dtls.cid JOIN dzongkhag_master ON dzongkhag_master.id = dessung_profile.dzongkhag_id JOIN qualifications_master ON qualifications_master.id=dessung_profile.qualification_id;"
+#result = db_connection.execute(sql_query)
+#result =pd.DataFrame(result)
+#result.rename(columns={8:'CID #',10:'DOB',11:'Email',14:'Name',27:'Programme',35:'DID',38:'Sex',47:'Dzongkhag',49:'Qualification'},inplace=True)
+#df=result[['CID #','Name','Programme','DID','Sex','Dzongkhag','Qualification']]
+
+
+# In[6]:
+
+
+result = pd.read_sql_query(sql_query, db_connection)
+df =pd.DataFrame(result)
+
+
+# In[7]:
 
 
 app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL, dbc.icons.FONT_AWESOME])
@@ -59,7 +75,7 @@ app.title = "DSP Dashboard"
 server = app.server
 
 
-# In[6]:
+# In[8]:
 
 
 app.layout= dbc.Container([
@@ -75,7 +91,7 @@ app.layout= dbc.Container([
 ])
 
 
-# In[7]:
+# In[9]:
 
 
 @app.callback(
@@ -87,9 +103,9 @@ def on_button_click(n):
     if n is None:
         fig = go.Figure()
     else:
-        g = df[['Sex']].value_counts().to_frame(name = 'number')
+        g = df[['gender']].value_counts().to_frame(name = 'number')
         g.reset_index(inplace = True)
-        fig = px.bar(g, x= 'Sex', y = 'number', color = 'Sex')
+        fig = px.bar(g, x= 'gender', y = 'number', color = 'gender')
     return fig
 
 
